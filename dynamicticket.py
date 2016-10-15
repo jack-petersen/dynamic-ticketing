ec8a74ea2f03b7a0d5a970b39252dc3eb91a26e8
jack-petersen <johnhenrypetersen@gmail.com>
2016-10-15 12:37:33 -0500

    First commit

---
+++ dynamicticket.py
@@ -0,0 +1,27 @@
+
+# THink
+# Establish lists of
+
+
+def calcEloSkill():
+    # Input: dictionary with elo ratings
+    # Output: a number that serves as the modifier
+    pass
+
+
+def calcStarPower():
+    # Input: A dictionary mapping Teams to their "stars" according to top 100 players in NFL
+    # Output: A number serving as modifier
+    pass
+
+def calcBigMarket():
+    # Input: A dictionary with net worth of teams, a dictionary with "Market sizes" of each team
+    # Output: A number serving as a modifier
+    pass
+
+def sameDivision():
+    # Input: A double dictionary with each team mapped to the other teams in its division
+    # Output: A number serving as a modifier
+    pass
+
+print "Jack"
