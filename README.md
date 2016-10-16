5c6e1712b199f9bec8f1245bc9a41d0063771ffc
jack-petersen <johnhenrypetersen@gmail.com>
2016-10-15 22:36:17 -0500

    added jersey sale logic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dynamic-ticketing
+
+# This is a program used to determine the modifier that should be applied
+# to tickets to minimize secondary market profit and keep the profits to
+# the primary ticket sellers (Jacksonville Jaguars).
--- dynamicticket.py
+++ dynamicticket.py
@@ -6,14 +6,25 @@ mediaMarketDict = {'denver': 4727, 'seattle': 4565, 'carolina': 8152, 'kansascit
             'detroit':8032, 'washington': 5853, 'philadelphia': 8688, 'atlanta':6462, 'indianapolis': 3266, 'losangeles':3986, 'baltimore':4248, 'nygiants':22421, 'neworleans':2635, 'oakland':10645, 'dallas':8071, 'chicago':10606, 'miami':1439,
             'sandiego':2683, 'sanfrancisco':10645, 'tampabay':4417, 'cleveland':4794, 'jacksonville':3660, 'tennessee':2667}
 
-stardict = {'denver': {'vonmiller':85, 'aqibtalib':66, 'demarcusware':64}, 'seattle': {'russellwilson':83, 'richardsherman':80, 'kamchancellor':68}, 'carolina': {'camnewton':100,'lukekuechly':93, 'gregolsen':62}, 'kansascity': {'justinhouston':74},
-            'arizona':{'carsonpalmer':88, 'patrickpeterson':82, 'larryfitzgerald':73, 'tyrannmathieu':72},
-            'newengland':{'tombrady': 99, 'robgronkowski':91}, 'pittsburgh':{'antoniobrown':97, 'benroethlisberger':79, 'leveonbell':59}, 'greenbay':{'aaronrodgers':94}, 'cincinnati':{'ajgreen':84, 'genoatkins':71, 'andydalton':65}, 'minnesota':{'adrianpeterson':95},
-            'nyjets':{'darellerevis':76, 'brandonmarshall':75, 'muhammadwilkerson': 61}, 'buffalo':{'leseanmccoy':31}, 'houston':{'jjwatt':98, 'deandrehopkins':81},'detroit':{'ezekielansah':57},'washington': {'joshnorman':89}, 'philadelphia': {},
-            'atlanta':{'juliojones':92}, 'indianapolis': {'andrewluck':8}, 'losangeles':{'aarondonald':86, 'toddgurley':78},
-            'baltimore':{'marshalyanda':63}, 'nygiants':{'odellbeckhamjr':90}, 'neworleans':{'drewbrees':70}, 'oakland':{'khalilmack':87},
-            'dallas':{'tyronsmith':58}, 'chicago':{'mattforte':10}, 'miami':{'suh':60}, 'sandiego':{'philiprivers':54}, 'sanfrancisco':{'navorrobowman':39}, 'tampabay':{'dougmartin':67}, 'cleveland':{'joethomas':77}, 'jacksonville':{'allenrobinson':69},
+stardict = {'denver': {'vonmiller':85, 'aqibtalib':66, 'demarcusware':64, 'chrisharrisjr':48, 'demaryiusthomas':38, 'tjward':32, 'emmanuelsanders':26}, 'seattle': {'russellwilson':83, 'richardsherman':80, 'kamchancellor':68, 'michaelbennett':41,
+            'earlthomas':34, 'dougbaldwin':28}, 'carolina': {'camnewton':100,'lukekuechly':93, 'gregolsen':62, 'thomasdavis':46, 'kawannshort':42, 'ryankalil':21, 'johnathanstewart':14},
+            'kansascity': {'justinhouston':74, 'ericberry':45, 'marcuspeters':35, 'carlosdunlap':30, 'jamaalcharles':25, 'derrickjohnson':20, 'alexsmith':19, 'tambahall':16, 'traviskelce':9, 'jeremymaclin':8},
+            'arizona':{'carsonpalmer':88, 'patrickpeterson':82, 'larryfitzgerald':73, 'tyrannmathieu':72, 'chandlerjones':52, 'calaiscampbell':29},
+            'newengland':{'tombrady': 99, 'robgronkowski':91, 'julianedelman':13}, 'pittsburgh':{'antoniobrown':97, 'benroethlisberger':79, 'leveonbell':59, 'cameronheyward':12}, 'greenbay':{'aaronrodgers':94, 'claymatthews':57, 'mikedaniels':5},
+            'cincinnati':{'ajgreen':84, 'genoatkins':71, 'andydalton':65, 'tylereifert':56, 'andrewwhitworth':33}, 'minnesota':{'adrianpeterson':95, 'harrisonsmith':27, 'linvaljoseph':24},
+            'nyjets':{'darellerevis':76, 'brandonmarshall':75, 'muhammadwilkerson': 61}, 'buffalo':{'leseanmccoy':31, 'sammywatkins':4, 'richieincognito':3}, 'houston':{'jjwatt':98, 'deandrehopkins':81},'detroit':{'ezekielansah':57},
+            'washington': {'joshnorman':89, 'trentwilliams':55, 'jordanreed':23, 'kirkcousins':15}, 'philadelphia': {'fletchercox':51},
+            'atlanta':{'juliojones':92, 'devontafreeman':50}, 'indianapolis': {'andrewluck':8}, 'losangeles':{'aarondonald':86, 'toddgurley':78},
+            'baltimore':{'marshalyanda':63}, 'nygiants':{'odellbeckhamjr':90, 'elimanning':53}, 'neworleans':{'drewbrees':70, 'cameronjordan':1}, 'oakland':{'khalilmack':87, 'reggienelson':40, 'derekcarr':1},
+            'dallas':{'tyronsmith':58, 'dezbryant':49}, 'chicago':{'mattforte':10}, 'miami':{'suh':60, 'reshadjones':36, 'jarvislandry':2}, 'sandiego':{'philiprivers':54}, 'sanfrancisco':{'navorrobowman':39},
+            'tampabay':{'dougmartin':67, 'lavontedavid':47, 'geraldmccoy':37}, 'cleveland':{'joethomas':77, 'garybarnidge':6}, 'jacksonville':{'allenrobinson':69, 'blakebortles':44, 'chrisivory':22, 'telvinsmith':17, 'allenhurns':11},
             'tennessee':{'delaniewalker':18}}
+
+jerseydict = {'denver': {'vonmiller':20}, 'seattle': {'russellwilson':19, '12thman':16}, 'carolina': {'camnewton':12, 'lukekuechly':3}, 'kansascity': {}, 'arizona':{},
+              'newengland':{'tombrady':23, 'robgronkowski':17, 'julianedelman':9}, 'pittsburgh':{'antoniobrown':22, 'benroethlisberger':10}, 'greenbay':{'aaronrodgers':14}, 'cincinnati':{}, 'minnesota':{}, 'nyjets':{}, 'buffalo':{}, 'houston':{},
+              'detroit':{}, 'washington': {}, 'philadelphia': {'carsonwentz':21}, 'atlanta':{}, 'indianapolis': {}, 'losangeles':{'toddgurley':1}, 'baltimore':{}, 'nygiants':{'odellbeckhamjr':25, 'elimanning':5},
+              'neworleans':{'drewbrees':8}, 'oakland':{'derekcarr':7, 'khalilmack':4}, 'dallas':{'ezekielelliot':18, 'dezbryant':15, 'dakprescott':13, 'jasonwitten':11}, 'chicago':{}, 'miami':{'jarvislandry':2},
+              'sandiego':{}, 'sanfrancisco':{'colinkaepernick':24, 'navorrobowman':6}, 'tampabay':{}, 'cleveland':{}, 'jacksonville':{}, 'tennessee':{}}
 
 divisiondict = {'seattle': ['arizona', 'losangeles', 'sanfrancisco'], 'arizona': ['seattle', 'sanfrancisco', 'losangeles'], 'sanfrancisco': ['seattle', 'arizona', 'losangeles'], 'losangeles': ['arizona', 'seattle', 'sanfrancisco'],
                 'minnesota': ['greenbay', 'detroit', 'chicago'], 'greenbay': ['minnesota', 'detroit', 'chicago'], 'detroit': ['greenbay', 'minnesota', 'chicago'], 'chicago': ['greenbay', 'detroit', 'minnesota'],
@@ -28,7 +39,7 @@ eloDict = {'denver': 1637, 'seattle': 1635, 'carolina': 1634, 'kansascity': 1621
             'detroit':1503, 'washington': 1496, 'philadelphia': 1488, 'atlanta':1486, 'indianapolis': 1484, 'losangeles':1479, 'baltimore':1475, 'nygiants':1469, 'neworleans':1464, 'oakland':1455, 'dallas':1446, 'chicago':1444, 'miami':1439,
             'sandiego':1438, 'sanfrancisco':1437, 'tampabay':1412, 'cleveland':1395, 'jacksonville':1389, 'tennessee':1349}
 
-varWeightDict = {'elo': 0.35, 'market': 0.1, 'division': .15, 'stars':.4 }
+varWeightDict = {'elo': 0.35, 'market': 0.15, 'division': .1, 'stars':.4 }
 
 
 
@@ -45,7 +56,7 @@ def calcEloSkill(eloDict, ht, vt):
 
 
 
-def calcStarPower(stardict, ht, vt):
+def calcStarPower(stardict, jerseydict, ht, vt):
     # Input: A dictionary mapping Teams to their "stars" according to top 100 players in NFL, a home team, an away team
     # Output: A number serving as modifier
 
@@ -54,6 +65,7 @@ def calcStarPower(stardict, ht, vt):
     # Matt Forte is an exception, but we will let it slide because the bears are terrible.
 
     # Iteratre through dict: for each team, a sum of their star power
+    # The first part of star power
     totalstarsum =0
     starsum = {}
     for i in stardict:
@@ -63,9 +75,22 @@ def calcStarPower(stardict, ht, vt):
             totalstarsum += stardict[i][j]
     averagestarsum = totalstarsum / 32.0
     starModifier = starsum[vt] * 1.0 / averagestarsum
-    return starModifier
+
+    # The second part of star power
+    jerseystarsum = {}
+    totaljerseysum = 0
+    for i in jerseydict:
+        jerseystarsum[i] = 0
+        for j in jerseydict[i]:
+            jerseystarsum[i] += jerseydict[i][j]
+            totaljerseysum+= jerseydict[i][j]
+    averagejerseysum = totaljerseysum / 32.0
+    jerseyModifier = jerseystarsum[vt] * 1.0 / averagejerseysum
 
 
+    newStarModifier = (jerseyModifier * 0.5) + (starModifier *0.5)
+
+    return newStarModifier
 
 
 def calcMarketSize(mediaMarketDict, ht, vt):
